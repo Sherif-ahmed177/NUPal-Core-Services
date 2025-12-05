3cc1449069f0b0684e97b40dd270487b276dd2a3
Sherif-ahmed177 <s.ahmed2268@nu.edu.eg>
2025-12-05 15:17:51 +0200

    Create Dependece Injection

---
+++ NUPAL.Core.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using NUPAL.Core.Application.Interfaces;
+using Nupal.Core.Infrastructure.Repositories;
+
+namespace NUPAL.Core.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var mongoUrl = configuration.GetValue<string>("MONGO_URL")
+                           ?? Environment.GetEnvironmentVariable("MONGO_URL");
+
+            services.AddSingleton<IMongoClient>(_ => new MongoClient(mongoUrl));
+            services.AddSingleton<IMongoDatabase>(sp =>
+            {
+                var client = sp.GetRequiredService<IMongoClient>();
+                return client.GetDatabase("nupal");
+            });
+
+            services.AddScoped<IStudentRepository, StudentRepository>();
+
+            return services;
+        }
+    }
+}
